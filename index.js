152bec4005e266df145041b7279f47275b545213
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-03 11:03:47 -0400

    commit tree 788a1c19dd64e8c53f5cbfbd9778cca6360f7681

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map(firstLetter => {
+    const rearrange = firstLetter.split(' '); // split the char
+    const capitalizeSplits = 
+      rearrange.map( token => token.charAt(0).toUpperCase() + token.slice(1) );
+      const result = capitalizeSplits.join(' ');
+      return result;
+  });
 }
+
+// output: first letter of each word capitalize
+//For example, 'what does the this keyword mean?' should become 'What Does The This Keyword Mean?'
+
+
+//How can we "iterate" through individual words in a string?
+//Can we execute an interation inside an iteration? How?
+//How can we capitalize just the first letter in a word?
